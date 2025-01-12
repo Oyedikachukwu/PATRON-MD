2ebb1b955031893c111448aa3826a891d4ae8b87
Oyedikachukwu <obiohadestiny8@gmail.com>
2025-01-12 11:12:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "palmpay"
 
-global.banknumber = "8133729715"
+global.banknumber = "9075750995"
 
-global.bankowner = "FADARE"
+global.bankowner = "Cynthia ugbomma"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
